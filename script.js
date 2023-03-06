52134efbe3e314e588babd24e9eb32a9a4f02f91
masskalauni <103063472+masskalauni@users.noreply.github.com>
2023-03-07 00:40:12 +0545

    my website
    my website

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load',function(){
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+  });
+
+  $('.portfolio .button-container .btn').click(function(){
+
+    let filter = $(this).attr('data-filter');
+
+    if(filter == 'all'){
+      $('.portfolio .image-container .box').show('400')
+    }else{
+      $('.portfolio .image-container .box').not('.'+filter).hide('200');
+      $('.portfolio .image-container .box').filter('.'+filter).show('400');
+    }
+
+  });
+
+  $('#theme-toggler').click(function(){
+    $(this).toggleClass('fa-sun');
+    $('body').toggleClass('dark-theme');
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500,
+      'linear'
+    );
+
+  });
+
+});
+function sure(){
+  var pk=confirm("Are You Sure Want To Download The CV?");
+  if(pk!=""){
+    window.open("file:///C:/Portfolio/Professional%20Resume%20Web%20Developer.pdf");
+    return true;
+  }
+  else{
+    window.open("#home");
+    return false;
+  }
+}
---
+++ webdesign.php
@@ -0,0 +1,51 @@
+
+
+
+
+
+<?php
+
+require_once'navbar.html';
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+Web design is an essential component of creating a successful website that attracts and retains visitors. A well-designed website can help businesses showcase their brand, communicate their message effectively, and improve user engagement and conversion rates. In this blog, we will dive deeper into the world of web design and explore its different elements.
+
+Layout
+The layout of a website refers to the arrangement of its content. The layout should be organized in a way that is easy to navigate and aesthetically pleasing. A good layout should have a clear hierarchy of information, with the most important content prominently displayed. The use of white space and visual elements such as images and icons can help make the layout more appealing and easier to navigate.
+
+Color
+Color is an important element of web design, as it can evoke specific emotions and help establish the brand identity. The color scheme should be chosen carefully, taking into account the brand's personality and the audience's preferences. Bold and bright colors can create a sense of energy and excitement, while softer and more muted colors can convey a sense of calm and sophistication.
+
+Typography
+Typography refers to the choice of fonts used on a website. The typography should be legible and easy to read, while also conveying the brand's personality. The use of different font styles, sizes, and colors can help create visual interest and highlight important information.
+
+Graphics and Images
+Graphics and images can be used to enhance the visual appeal of a website and convey its message effectively. The use of high-quality images and videos can help capture the user's attention and keep them engaged. Graphics and icons can also be used to create visual interest and help organize the content.
+
+Navigation
+Navigation is a critical design element that enables users to navigate the website easily and find the information they need. The navigation should be intuitive and easy to use, with clear labels and a logical structure. The use of drop-down menus and breadcrumbs can help make the navigation more efficient.
+
+User Experience (UX) and User Interface (UI) Design
+UX design involves creating a positive experience for website visitors by making the website easy to navigate and providing clear and concise information. UI design focuses on the visual and interactive elements of a website, such as buttons, forms, and menus. A good UX/UI design can improve user engagement and conversion rates.
+
+In conclusion, web design is a critical element of creating a successful website that attracts and retains visitors.
+ By using design principles, tools, and technologies, web designers can create visually appealing, user-friendly websites that effectively communicate their message and establish their brand identity. A good web design should have a clear layout, a well-chosen color scheme, legible typography, high-quality graphics and images, intuitive navigation, and a positive UX/UI design.
